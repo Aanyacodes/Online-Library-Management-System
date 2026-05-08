b988355199f975c651502e38ac44f0375deaa877
Aanyacodes <87720390+Aanyacodes@users.noreply.github.com>
2026-05-08 13:47:33 +0530

    Add files via upload

---
+++ src/main/java/com/library/createDBConnection.java
@@ -0,0 +1,15 @@
+package com.library;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class createDBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/LMSAdmin";
+    private static final String USER = "LMSAdmin";
+    private static final String PASSWORD = "Lm$Adm!n@2026";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
